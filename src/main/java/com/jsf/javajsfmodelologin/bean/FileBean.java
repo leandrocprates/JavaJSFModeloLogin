99ffc658dd3cdd4edea50f1ca91d2f5a43f843a4
leandro prates <leandro.prates@gmail.com>
2019-08-12 22:17:22 -0300

    teste upload de arquivos

---
+++ src/main/java/com/jsf/javajsfmodelologin/bean/FileBean.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jsf.javajsfmodelologin.bean;
+
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+
+@ManagedBean
+public class FileBean {
+
+    private UploadedFile file;
+
+    public void upload() {
+        if(file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+    public UploadedFile getFile() {
+        return file;
+    }
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+    
+    
+    public void handleFileUpload(FileUploadEvent event) {
+        System.out.println("Arquivo carregado: " + event.getFile().getFileName());
+        System.out.println( Thread.currentThread().getId() +"-"+ Thread.currentThread().getName());
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    
+    
+}
